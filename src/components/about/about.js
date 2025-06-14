a3de6dcfd442fe5b659c5a69399d40b98b4f5a66
mahmoud2391997 <mahmoudmelsaid1@gmail.com>
2025-06-14 06:56:38 -0700

    converting to rtl

--- src/components/about/about.js
+++ src/components/about/about.js
@@ -1,73 +1,79 @@
 import React from 'react';
 import CountUp from 'react-countup';
 
+// Replace with real imports or use actual URLs if using from online source
+import Abd1 from '../../images/doctors/1.jpg';
+import Abd2 from '../../images/doctors/2.jpg';
+import Abd3 from '../../images/doctors/3.jpg';
+import Abd4 from '../../images/doctors/4.jpg';
+import sine from '../../images/signeture.png';
 
-// image
-import Ab1 from '../../images/about.jpg'
-import Abd1 from '../../images/doctors/1.jpg'
-import Abd2 from '../../images/doctors/2.jpg'
-import Abd3 from '../../images/doctors/3.jpg'
-import Abd4 from '../../images/doctors/4.jpg'
-import sine from '../../images/signeture.png'
-
-const about = (props) => {
-    return (
-        <section className={"" + props.hclass}>
-            <div className="container">
-                <div className="row align-items-center">
-                    <div className="col-lg-6 col-12">
-                        <div className="about_left">
-                            <div className="image">
-                                <img src={Ab1} alt="" />
-                                    <span className="round-on"></span>
-                                    <span className="round-two"></span>
-                                    <div className="award">
-                                        <div className="icon">
-                                            <i className="flaticon-cup"></i>
-                                        </div>
-                                        <div className="text">
-                                        <h2><CountUp end={25} enableScrollSpy />+</h2>
-                                            <p>Years Of Experience</p>
-                                        </div>
-                                    </div>
-                                    <div className="doctors">
-                                        <ul>
-                                        <li><img src={Abd1} alt="" /></li>
-                                        <li><img src={Abd2} alt="" /></li>
-                                        <li><img src={Abd3} alt="" /></li>
-                                        <li><img src={Abd4} alt="" /></li>
-                                            <li><span>95+</span></li>
-                                        </ul>
-                                        <h4>Available Doctors</h4>
-                                    </div>
-                            </div>
-                        </div>
-                    </div>
-                    <div className="col-lg-6 col-12">
-                        <div className="content">
-                            <h2>About Medically</h2>
-                            <h3>Your Smile & Happiness Is Our Mission</h3>
-                            <p>Our health and hospital policy encompasses the strategies, guidelines, and practices that
-                                technology companies use to achieve their goals and objectives. The policies may vary
-                                depending on the company's size, market position, and competitive landscape. Commodo
-                                erat amet vitae consectetur consectetur feugiat.</p>
-                            <p>Tellus viverra eu risus ut ipsum magna sed odio elit. Sed sem purus tincidunt
-                                condimentum amet condimentum massa. Nunc vel nascetur id cras.</p>
-                            <div className="ceo">
-                                <div>
-                                    <h4>Savannah Nguyen</h4>
-                                    <span>CEO & Founder of Madically</span>
-                                </div>
-                                <div>
-                                    <img src={sine} alt="" />
-                                </div>
-                            </div>
-                        </div>
-                    </div>
+const About = (props) => {
+  return (
+    <section className={props.hclass || ""} dir="rtl">
+      <div className="container">
+        <div className="row align-items-center">
+          <div className="col-lg-6 col-12">
+            <div className="about_left">
+              <div className="image position-relative">
+                <img
+                  src="https://medically-react.wpolive.com/static/media/1.f83d52cba0c04965b7cd.png"
+                  alt="عيادة ماسترز"
+                  style={{ maxWidth: "100%", borderRadius: "10px" }}
+                />
+                <span className="round-on"></span>
+                <span className="round-two"></span>
+                <div className="award">
+                  <div className="icon">
+                    <i className="flaticon-cup"></i>
+                  </div>
+                  <div className="text">
+                    <h2>
+                      <CountUp end={25} enableScrollSpy />+
+                    </h2>
+                    <p>سنوات من الخبرة</p>
+                  </div>
                 </div>
+                <div className="doctors">
+                  <ul>
+                    <li><img src={Abd1} alt="دكتور 1" /></li>
+                    <li><img src={Abd2} alt="دكتور 2" /></li>
+                    <li><img src={Abd3} alt="دكتور 3" /></li>
+                    <li><img src={Abd4} alt="دكتور 4" /></li>
+                    <li><span>+95</span></li>
+                  </ul>
+                  <h4>عدد الأطباء المتوفرين</h4>
+                </div>
+              </div>
             </div>
-        </section>
-    );
+          </div>
+
+          <div className="col-lg-6 col-12">
+            <div className="content">
+              <h2>ماسترز خبراء لجمالك وصحتك</h2>
+              <h3>اهتمي ببشرتك... مع أفضل أطباء الجلدية والتجميل في عيادات ماسترز</h3>
+              <p>
+                جلسات نضارة، علاج لحب الشباب، تساقط الشعر، تقشير، بوتوكس، فيلر، ليزر، وكل ما يخص العناية بجمالك.
+              </p>
+              <p>
+                نحن نؤمن أن الجمال يبدأ من الصحة. لذلك، في عيادات ماسترز نقدم خدمات متكاملة باستخدام أحدث التقنيات وأفضل الكفاءات.
+              </p>
+              <div className="ceo d-flex align-items-center justify-content-between">
+                <div>
+                  <h4>سافانا نغوين</h4>
+                  <span>المدير التنفيذي ومؤسس عيادات ماسترز</span>
+                </div>
+                <div>
+                  <img src={sine} alt="توقيع المدير" style={{ maxHeight: "50px" }} />
+                </div>
+              </div>
+            </div>
+          </div>
+
+        </div>
+      </div>
+    </section>
+  );
 };
 
-export default about;
+export default About;
